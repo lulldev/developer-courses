cb98efe1b49ae833ffb4aaeb22517ff4eeab386c
Ivan Kalagin <ivan.kalagin@inplat.ru>
2021-11-21 16:22:32 +0300

    Add publish by node.js

---
+++ publish.js
@@ -0,0 +1,15 @@
+var glob = require("glob");
+const { execSync } = require("child_process");
+
+glob("courses/**/*.md", function (err, files) {
+  files.forEach((file) => {
+    console.log(file);
+    execSync(`./node_modules/.bin/marp ${file} -o docs/${file}.pdf`);
+  });
+
+  execSync("git add ./docs/**/*");
+  execSync(
+    `git commit -m "[Update]: Обновление материалов презентаций (${new Date()})"`
+  );
+  execSync("git push");
+});
