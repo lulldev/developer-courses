71983b54737b06cf121d64bfbfc930dfcde2ddb0
Ivan Kalagin <ivan.kalagin@inplat.ru>
2021-11-21 15:52:28 +0300

    publish presentations

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@
 
 marp ./courses/software-arch/lection1/lection-1-paradigms-live.md -o docs/software-arch-course/lection-1-paradigms-live.pdf --allow-local-files
 
+git add ./docs/**/*
+
 git commit -m "publish presentations"
 
 git push
