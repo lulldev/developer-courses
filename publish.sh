2a92ac254f81834ee37dc572b466da3f5865fac2
Ivan Kalagin <ivan.kalagin@inplat.ru>
2021-11-21 15:59:50 +0300

    Refactor

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-marp ./courses/software-arch/lection1/lection-1-paradigms-live.md -o docs/software-arch-course/lection-1-paradigms-live.pdf --allow-local-files
+marp ./courses/software-arch/lection1/lection-1-paradigms-live.md -o docs/software-arch-course/lection-1-paradigms-live.pdf
 
 git add ./docs/**/*
 
-git commit -m "publish presentations"
+git commit -m "publish presentations ($(date))"
 
 git push
